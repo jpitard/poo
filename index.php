649578bc5dca42c4f194a1a4aa10b62624e25167
jpitard <johnny.pitard@gmail.com>
2017-01-03 11:00:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'vendor/autoload.php';
+use AppBundle\Controller\DefaultController;
+
+ $test = new DefaultController();
+
+var_dump($test);
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace AppBundle\Controller;
+
+/**
+* 
+*/
+class DefaultController 
+{
+	
+	public function __construct()
+	{
+		echo "instanciation de la classe DefaultController";
+	}
+}
